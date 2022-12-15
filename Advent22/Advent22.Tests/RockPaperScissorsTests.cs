01cd31a0d324e5fb303f2474fa7e6313a76bc872
Thomas <thombsaway@gmail.com>
2022-12-16 10:10:30 +1100

    Implemented RockPaperScissor solution and tests, passing advent.

---
+++ Advent22/Advent22.Tests/RockPaperScissorsTests.cs
@@ -0,0 +1,62 @@
+﻿using Xunit;
+using Advent22;
+
+namespace Advent22.Tests
+{
+    public class RockPaperScissorsTests
+    {
+        private readonly RockPaperScissors _rockPaperScissors;
+        private readonly string _testData;
+        private readonly string _data;
+
+        public RockPaperScissorsTests()
+        {
+            _rockPaperScissors = new RockPaperScissors();
+
+            _testData = @"TestData\\TotalScoreTestData.txt";
+            _data = @"TestData\\TotalScoreData.txt";
+        }
+
+        [Fact]
+        public void TotalScore_WhenPassedTestData_ReturnsExpectedTotal()
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines(_testData);
+            int expected = 15;
+
+            // Act
+            var actual = _rockPaperScissors.TotalScore(lines);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TotalScore_WhenPassedData_ReturnsExpectedTotal()
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines(_data);
+            int expected = 13682;
+
+            // Act
+            var actual = _rockPaperScissors.TotalScore(lines);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TotalScoreTwo_WhenPassedData_ReturnsExpectedTotal()
+        {
+            // Arrange
+            string[] lines = System.IO.File.ReadAllLines(_data);
+            int expected = 12881;
+
+            // Act
+            var actual = _rockPaperScissors.TotalScoreTwo(lines);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Advent22/Advent22/RockPaperScissors.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Advent22
+{
+    public class RockPaperScissors
+    {
+
+        public int TotalScore(string[] lines)
+        {
+            int score = 0;
+
+            int rockScore = 1;
+            int paperScore = 2;
+            int scissorScore = 3;
+
+            int win = 6;
+            int draw = 3;
+            int loss = 0;
+
+            foreach (string line in lines)
+            {
+                char x = line[0];
+                char y = line[2];
+
+                // If they've picked rock
+                if(x == 'A')
+                {
+                    if (y == 'X') score += rockScore + draw;
+                    if (y == 'Y') score += paperScore + win;
+                    if (y == 'Z') score += scissorScore + loss;
+                }
+
+                // If they've picked paper
+                if (x == 'B')
+                {
+                    if (y == 'X') score += rockScore + loss;
+                    if (y == 'Y') score += paperScore + draw;
+                    if (y == 'Z') score += scissorScore + win;
+                }
+
+                // If they've picked scissors
+                if (x == 'C')
+                {
+                    if (y == 'X') score += rockScore + win;
+                    if (y == 'Y') score += paperScore + loss;
+                    if (y == 'Z') score += scissorScore + draw;
+                }
+            }
+
+            return score;
+        }
+
+        public int TotalScoreTwo(string[] lines)
+        {
+            int score = 0;
+
+            int rockScore = 1;
+            int paperScore = 2;
+            int scissorScore = 3;
+
+            int win = 6;
+            int draw = 3;
+            int loss = 0;
+
+            foreach (string line in lines)
+            {
+                char x = line[0];
+                char y = line[2];
+
+                // We need to lose
+                if (y == 'X')
+                {
+                    score += loss;
+
+                    if (x == 'A') score += scissorScore;
+                    if (x == 'B') score += rockScore;
+                    if (x == 'C') score += paperScore;
+                }
+
+                // We need to draw
+                if (y == 'Y')
+                {
+                    score += draw;
+
+                    if (x == 'A') score += rockScore;
+                    if (x == 'B') score += paperScore;
+                    if (x == 'C') score += scissorScore;
+                }
+
+                // We need to win
+                if (y == 'Z')
+                {
+                    score += win;
+
+                    if (x == 'A') score += paperScore;
+                    if (x == 'B') score += scissorScore;
+                    if (x == 'C') score += rockScore;
+                }
+            }
+
+            return score;
+        }
+    }
+}
