5f3f92471085093f318e5fc405d3b4deab19fc93
Thomas <thombsaway@gmail.com>
2022-12-20 12:12:55 +1100

    Refactored getPriorities tests, using theory to pass more test cases.

--- Advent22/Advent22.Tests/RucksackReorganisationTests.cs
+++ Advent22/Advent22.Tests/RucksackReorganisationTests.cs
@@ -32,56 +32,33 @@ namespace Advent22.Tests
         }
 
         [Fact]
-        public void GetPriorities_WhenDictPasseda_Returns1()
-        {
-            // Arrange
-            int expected = 1;
-
-            // Act
-            var dict = _rucksackReorganisation.GetPriorities();
-            var actual = dict['a'];
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void GetPriorities_WhenDictPassedz_Returns26()
-        {
-            // Arrange
-            int expected = 26;
-
-            // Act
-            var dict = _rucksackReorganisation.GetPriorities();
-            var actual = dict['z'];
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void GetPriorities_WhenDictPassedA_Returns27()
-        {
-            // Arrange
-            int expected = 27;
-
-            // Act
-            var dict = _rucksackReorganisation.GetPriorities();
-            var actual = dict['A'];
-
-            // Assert
-            Assert.Equal(expected, actual);
-        }
-
-        [Fact]
-        public void GetPriorities_WhenDictPassedZ_Returns52()
+        public void GetPriorities_WhenCalled_ReturnsDictionaryLength52()
         {
             // Arrange
             int expected = 52;
 
             // Act
             var dict = _rucksackReorganisation.GetPriorities();
-            var actual = dict['Z'];
+            var actual = dict.Count;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData('a', 1)]
+        [InlineData('z', 26)]
+        [InlineData('A', 27)]
+        [InlineData('Z', 52)]
+        [InlineData('p', 16)]
+        [InlineData('L', 38)]
+        public void GetPrioritiesDictionary_WhenPassedCharacter_ReturnsExpectedValue(char input, int expected)
+        {
+            // Arrange
+
+            // Act
+            var dict = _rucksackReorganisation.GetPriorities();
+            var actual = dict[input];
 
             // Assert
             Assert.Equal(expected, actual);
